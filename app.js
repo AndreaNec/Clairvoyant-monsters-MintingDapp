bd86547f02f73ff1bb3f9a21d2d9eeb1d3e4fd28
AndreaNec <100475101+AndreaNec@users.noreply.github.com>
2022-04-25 12:39:21 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,167 @@
+
+// Setup End Date for Countdown (getTime == Time in Milleseconds)
+let launchDate = new Date("Apr 30, 2022 00:00:00").getTime();
+
+// Setup Timer to tick every 1 second
+let timer = setInterval(tick, 1000);
+
+function tick () {
+  // Get current time
+  let now = new Date().getTime();
+  // Get the difference in time to get time left until reaches 0
+  let t = launchDate - now;
+
+  // Check if time is above 0
+  if (t > 0) {
+    // Setup Days, hours, seconds and minutes
+    // Algorithm to calculate days...
+    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+    // prefix any number below 10 with a "0" E.g. 1 = 01
+    if (days < 10) { days = "0" + days; }
+    
+    // Algorithm to calculate hours
+    let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    if (hours < 10) { hours = "0" + hours; }
+
+    // Algorithm to calculate minutes
+    let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    if (mins < 10) { mins = "0" + mins; }
+
+    // Algorithm to calc seconds
+    let secs = Math.floor((t % (1000 * 60)) / 1000);
+    if (secs < 10) { secs = "0" + secs; }
+
+    // Create Time String
+    let time = `${days} : ${hours} : ${mins} : ${secs}`;
+
+    // Set time on document
+    document.querySelector('.countdown').innerText = time;
+    document.querySelector('.countdownfooter').innerText = time;
+  }
+}
+
+
+
+//Roadmap scroll
+var content = document.getElementById('content'),
+    scrollStep = 200;
+
+document.getElementById('right-button').addEventListener('click', function(e) {
+  e.preventDefault();
+  let sl = content.scrollLeft,
+      cw = content.scrollWidth;
+	
+  if ((sl + scrollStep) >= cw) {
+    content.scrollTo(cw, 0);
+  } else {
+    content.scrollTo((sl + scrollStep), 0);
+  }
+});
+
+document.getElementById('left-button').addEventListener('click', function(e) {
+  e.preventDefault();
+  let sl = content.scrollLeft;
+	
+  if ((sl - scrollStep) <= 0) {
+    content.scrollTo(0, 0);
+  } else {
+    content.scrollTo((sl - scrollStep), 0);
+  }
+});
+
+function slide(direction){
+  var container = document.getElementById('container');
+  scrollCompleted = 0;
+  var slideVar = setInterval(function(){
+      if(direction == 'left'){
+          container.scrollLeft -= 10;
+      } else {
+          container.scrollLeft += 10;
+      }
+      scrollCompleted += 10;
+      if(scrollCompleted >= 100){
+          window.clearInterval(slideVar);
+      }
+  }, 50);
+}
+
+
+// duration of scroll animation
+var scrollDuration = 300;
+// paddles
+var leftPaddle = document.getElementsByClassName('left-paddle');
+var rightPaddle = document.getElementsByClassName('right-paddle');
+// get items dimensions
+var itemsLength = $('.item').length;
+var itemSize = $('.item').outerWidth(true);
+// get some relevant size for the paddle triggering point
+var paddleMargin = 20;
+
+// get wrapper width
+var getMenuWrapperSize = function() {
+	return $('.menu-wrapper').outerWidth();
+}
+var menuWrapperSize = getMenuWrapperSize();
+// the wrapper is responsive
+$(window).on('resize', function() {
+	menuWrapperSize = getMenuWrapperSize();
+});
+// size of the visible part of the menu is equal as the wrapper size 
+var menuVisibleSize = menuWrapperSize;
+
+// get total width of all menu items
+var getMenuSize = function() {
+	return itemsLength * itemSize;
+};
+var menuSize = getMenuSize();
+// get how much of menu is invisible
+var menuInvisibleSize = menuSize - menuWrapperSize;
+
+// get how much have we scrolled to the left
+var getMenuPosition = function() {
+	return $('.menu').scrollLeft();
+};
+
+// finally, what happens when we are actually scrolling the menu
+$('.menu').on('scroll', function() {
+
+	// get how much of menu is invisible
+	menuInvisibleSize = menuSize - menuWrapperSize;
+	// get how much have we scrolled so far
+	var menuPosition = getMenuPosition();
+
+	var menuEndOffset = menuInvisibleSize - paddleMargin;
+
+	// show & hide the paddles 
+	// depending on scroll position
+	if (menuPosition <= paddleMargin) {
+		$(leftPaddle).addClass('hidden');
+		$(rightPaddle).removeClass('hidden');
+	} else if (menuPosition < menuEndOffset) {
+		// show both paddles in the middle
+		$(leftPaddle).removeClass('hidden');
+		$(rightPaddle).removeClass('hidden');
+	} else if (menuPosition >= menuEndOffset) {
+		$(leftPaddle).removeClass('hidden');
+		$(rightPaddle).addClass('hidden');
+}
+
+	// print important values
+	$('#print-wrapper-size span').text(menuWrapperSize);
+	$('#print-menu-size span').text(menuSize);
+	$('#print-menu-invisible-size span').text(menuInvisibleSize);
+	$('#print-menu-position span').text(menuPosition);
+
+});
+
+// scroll to left
+$(rightPaddle).on('click', function() {
+	$('.menu').animate( { scrollLeft: menuInvisibleSize}, scrollDuration);
+});
+
+// scroll to right
+$(leftPaddle).on('click', function() {
+	$('.menu').animate( { scrollLeft: '0' }, scrollDuration);
+});
+
+
