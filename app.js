7f5a66e067c7f79f34ed153eaea4e6124d636462
AndreaNec <100475101+AndreaNec@users.noreply.github.com>
2022-04-25 14:47:10 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 // Setup End Date for Countdown (getTime == Time in Milleseconds)
-let launchDate = new Date("Apr 30, 2022 00:00:00").getTime();
+let launchDate = new Date("Apr 14, 2022 00:00:00").getTime();
 
 // Setup Timer to tick every 1 second
 let timer = setInterval(tick, 1000);
